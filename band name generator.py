0be3bbe6b3d1b6375e8b253d24cbc6f6d72ae4ab
N Dhruva Ballal <dhruvaballal30@gmail.com>
2026-03-17 23:50:44 +0530

    day 1- Band name generator completed

---
+++ band name generator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city = input("which city did yoou grow up?\n")
+pet = input("what is the name of the pet?\n")
+print("your band name could be:" + city + " " + pet)
