58385ee452806e9f58b0c65a5ab571792f7b53ae
N Dhruva Ballal <dhruvaballal30@gmail.com>
2026-04-06 22:56:45 +0530

    Day 15 - coffee machine project completed
    coffee machine project

---
+++ coffee machine.py
@@ -0,0 +1,59 @@
+
+#todo check resources sufficient to make drink order
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+#ask the user what would  you like to drink
+ask = input("What would you like? (espresso/latte/cappucino):")
+report = MENU[""]
+
+
+
+#turn off the coffee vending machine by typing off
+
+
+
+#print report about how much quantities the coffee ingredients contains
+
+
+#checkresources suffiecient or not
+
+
+#process coins
+
+
+#check transaction successful?
+
+
+
+#make coffee
