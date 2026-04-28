23be1e3578de35adbbb59811918e9e86e296bbf5
N Dhruva Ballal <dhruvaballal30@gmail.com>
2026-04-28 20:35:00 +0530

    Snake game updated version
    Here is the updated version of snake game, in this project we have a new feature where there is highscore scoreboard and it stores the highscore even after exiting the game. sanke_game.py is the main file

--- snake.py
+++ snake.py
@@ -25,6 +25,14 @@ class Snake:
         new_segment.penup()
         new_segment.goto(positions)
         self.segments.append(new_segment)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
